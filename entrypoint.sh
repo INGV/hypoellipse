fa93720aa69b0e48fa9d67e1c9d78a5512222489
Valentino Lauciani <valentino.lauciani@ingv.it>
2019-03-08 20:02:04 +0100

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,31 @@ DIR_DATA=/opt/data
 if [ -d ${DIR_DATA} ]; then
 	
 	# 
-		if [ -f ${DIR_DATA}/${1} ]; then
+	if [ -d ${DIR_DATA}/output ]; then
+		DIR_OUTPUT=${DIR_DATA}/output
+		rm ${DIR_OUTPUT}/*
+	else 
+		mkdir ${DIR_OUTPUT}/
+	fi
+
+	#
+	if [ -d ${DIR_DATA}/input ]; then
+		DIR_INPUT=${DIR_DATA}/input
+		if [ -f ${DIR_INPUT}/${1} ]; then
 			FILE_INPUT=${1}
-			cd ${DIR_DATA}
-			/opt/hypoellipse//hypoellipse < ${FILE_INPUT}
+			cd ${DIR_DATA}/input
+			/opt/hypoellipse/hypoellipse < ${FILE_INPUT}
 		else
-			echo " The \"${DIR_DATA}/${1}\" doesn't exist."
+			echo " The \"${DIR_INPUT}/${1}\" doesn't exist."
 			echo ""
 			exit 1
 		fi
+	else
+		echo " the \"${DIR_DATA}/input\" doesn't exist."
+		echo ""
+		exit 1
+	fi
+
 else
 	echo " the \"${DIR_DATA}\" doesn't exist."
         echo ""
